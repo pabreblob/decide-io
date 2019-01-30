8140c2ba68fe2750f16d194c01cdba7cd82cf038
Carlos Manuel Cabello Colmenares <carcabcol@alum.us.es>
2019-01-30 12:44:11 +0000

    Adapting tests new test for the borda count reimplementation Issue #19 and #20 30/01/2019 Issue status: Closed

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -40,6 +40,7 @@ class PostProcTestCase(APITestCase):
 
         values = response.json()
         self.assertEqual(values, expected_result)
+
     def test_weighted(self):
         data = {
             'type': 'WEIGHTED',
@@ -167,63 +168,90 @@ class PostProcTestCase(APITestCase):
     def test_borda1(self):
         data = {
             'type': 'BORDA',
-            'options': {
-                'choices': ['a', 'b', 'c', 'd', 'e'],
-                'votes': [['d', 'b', 'e', 'c', 'a'],
-                          ['a', 'd', 'b', 'c', 'e'],
-                          ['a', 'd', 'e', 'c', 'b']]
-            }
+            'options': [
+                { 'option': 'Option 1', 'number': 1, 'votes': 10},
+                { 'option': 'Option 2', 'number': 2, 'votes': 20},
+                { 'option': 'Option 3', 'number': 3, 'votes': 25},
+                { 'option': 'Option 4', 'number': 4, 'votes': 15},
+                { 'option': 'Option 5', 'number': 5, 'votes': 30},
+                { 'option': 'Option 6', 'number': 6, 'votes': 5}
+            ]
         }
 
-        expected = {'a': 11, 'b': 8, 'c': 6, 'd': 13, 'e': 7}
+        expected_result = [
+            {'option': 'Option 5', 'number': 5, 'votes': 30, 'postproc': 5.0},
+            {'option': 'Option 3', 'number': 3, 'votes': 25, 'postproc': 4.16667},
+            {'option': 'Option 2', 'number': 2, 'votes': 20, 'postproc': 3.33333},
+            {'option': 'Option 4', 'number': 4, 'votes': 15, 'postproc': 2.5},
+            {'option': 'Option 1', 'number': 1, 'votes': 10, 'postproc': 1.66667},
+            {'option': 'Option 6', 'number': 6, 'votes': 5, 'postproc': 0.83333}
+        ]
 
         response = self.client.post('/postproc/', data, format='json')
         self.assertEqual(response.status_code, 200)
 
         values = response.json()
-        self.assertEqual(values, expected)
+        self.assertEqual(values, expected_result)
 
 
     def test_borda2(self):
         data = {
             'type': 'BORDA',
-            'options': {
-                'choices': ['a', 'b', 'c', 'd', 'e'],
-                'votes': [['d', 'b', 'c', 'a', 'e'],
-                          ['e', 'd', 'c', 'b', 'a'],
-                          ['b', 'c', 'd', 'a', 'e'],
-                          ['a', 'e', 'd', 'c', 'b'],
-                          ['b', 'e', 'd', 'c', 'a']]
-            }
+            'options': [
+                { 'option': 'Option 1', 'number': 1, 'votes': 22},
+                { 'option': 'Option 2', 'number': 2, 'votes': 22},
+                { 'option': 'Option 3', 'number': 3, 'votes': 16},
+            ]
         }
 
-        expected = {'a': 11, 'b': 17, 'c': 14, 'd': 18, 'e': 15}
+        expected_result = [
+                { 'option': 'Option 1', 'number': 1, 'votes': 22, 'postproc': 7.33333},
+                { 'option': 'Option 2', 'number': 2, 'votes': 22, 'postproc': 7.33333},
+                { 'option': 'Option 3', 'number': 3, 'votes': 16, 'postproc': 5.33333},
+        ]
 
         response = self.client.post('/postproc/', data, format='json')
         self.assertEqual(response.status_code, 200)
 
         values = response.json()
-        self.assertEqual(values, expected)
+        self.assertEqual(values, expected_result)
 
 
     def test_borda3(self):
         data = {
             'type': 'BORDA',
-            'options': {
-                'choices': ['a', 'b', 'c', 'd', 'e'],
-                'votes': [['d', 'b', 'e', 'c', 'a'],
-                          ['a', 'd', 'b', 'c', 'e'],
-                          ['a', 'd', 'e', 'c', 'b']]
-            }
+            'options': [
+                { 'option': 'Option 1', 'number': 1, 'votes': 185},
+                { 'option': 'Option 2', 'number': 2, 'votes': 175},
+                { 'option': 'Option 3', 'number': 3, 'votes': 165},
+                { 'option': 'Option 4', 'number': 4, 'votes': 155},
+                { 'option': 'Option 5', 'number': 5, 'votes': 145},
+                { 'option': 'Option 6', 'number': 6, 'votes': 165},
+                { 'option': 'Option 7', 'number': 7, 'votes': 165},
+                { 'option': 'Option 8', 'number': 8, 'votes': 165},
+                { 'option': 'Option 9', 'number': 9, 'votes': 165},
+                { 'option': 'Option 10', 'number': 10, 'votes': 165}
+            ]
         }
 
-        expected = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0}
+        expected_result = [
+                { 'option': 'Option 1', 'number': 1, 'votes': 185, 'postproc': 18.5},
+                { 'option': 'Option 2', 'number': 2, 'votes': 175, 'postproc': 17.5},
+                { 'option': 'Option 3', 'number': 3, 'votes': 165, 'postproc': 16.5},
+                { 'option': 'Option 6', 'number': 6, 'votes': 165, 'postproc': 16.5},
+                { 'option': 'Option 7', 'number': 7, 'votes': 165, 'postproc': 16.5},
+                { 'option': 'Option 8', 'number': 8, 'votes': 165, 'postproc': 16.5},
+                { 'option': 'Option 9', 'number': 9, 'votes': 165, 'postproc': 16.5},
+                { 'option': 'Option 10', 'number': 10, 'votes': 165, 'postproc': 16.5},
+                { 'option': 'Option 4', 'number': 4, 'votes': 155, 'postproc': 15.5},
+                { 'option': 'Option 5', 'number': 5, 'votes': 145, 'postproc': 14.5}
+        ]
 
         response = self.client.post('/postproc/', data, format='json')
         self.assertEqual(response.status_code, 200)
 
         values = response.json()
-        self.assertNotEqual(values, expected)
+        self.assertNotEqual(values, expected_result)
 
 
     def test_hondt(self):
